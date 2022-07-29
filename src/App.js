e390d7dacfc2bc37a84fcf82d6d1829631fd9ed4
Israr <israrahmedpk1999@gmail.com>
2022-07-29 07:59:43 +0500

    initialize project

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import React from 'react';
+import Header from "./components/Header"
+import Home from "./components/Home"
+import Menu from "./components/Menu"
+import About from './components/About';
+import Working from "./components/Working"
+import Testimonial from "./components/Testimonial"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <About />
+      <Working />
+      <Menu
+        pic1="pic1.jpg"
+        name1="Chicken Salad &nbsp; $12"
+        text1="Try it out its best"
+
+        pic2="pic2.jpg"
+        name2="Cheese Burger &nbsp; $15"
+        text2="Yummy food for hunger"
+
+        pic3="pic3.jpg"
+        name3="Toast &nbsp; $15"
+        text3="Best for Breakfast"
+
+        pic4="pic4.jpg"
+        name4="Vegetable Salad &nbsp; $09"
+        text4="Nice ingredients for health"
+
+        pic5="pic5.png"
+        name5="kabab &nbsp; $13"
+        text5="Nutrition rich food"
+
+        pic6="pic6.jpg"
+        name6="Healthy Salad &nbsp; $07"
+        text6="Best for health"
+
+      />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+      <div className="container mt-5" id='cont-2' >
+        <div className='row g-3'>
+          <div className='col-12 col-md-6 col-lg-6'>
+            <img src="rounded_corners.jpg" alt='Healthy Foodie' />
+          </div>
+          {/* Its actually first col-12 for mobile devices mean 12/12=1 which shows 1 coloumn grid and then we have for
+        medium screens that are tablets where two grids 12/6=2 and for the large screens 12/6=2 */}
+
+          <div className='col-12 col-md-6 col-lg-6'>
+            <div className="card border-0" style={{ marginTop: 50 }}>
+              <div className="card-body " style={{ textAlign: "center" }}>
+                <p style={{ color: "#FF9F0D" }}>About</p>
+                <h1 className="card-title" >Food is an important part <br /> of Balanced Diet</h1>
+                <p className="card-text-col-sm-3">Some quick example text to build on the card title and make up
+                  the bulk of the card's content build on the card title and make up build on the card title and make up </p>
+                <button type="button" class="btn btn-warning">Read More</button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+    </>
+  )
+}
