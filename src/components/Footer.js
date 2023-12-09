1f1628dbe045acf82cc29e77859809b40b394b8e
Israr Ahmed <israrahmedpk1999@gmail.com>
2023-12-09 15:17:32 +0500

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,58 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
     <div>
-
       <footer class="bg-white text-center text-black mt-5 ">
         <div class="container p-4">
           <section class="mb-4">
-            <a class="btn btn-outline-black btn-floating m-1" href="#!" role="button"
-            ><i class="fab fa-facebook-f"></i
-            ></a>
+            <a
+              class="btn btn-outline-black btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i class="fab fa-facebook-f"></i>
+            </a>
 
-            <a class="btn btn-outline-black btn-floating m-1" href="#!" role="button"
-            ><i class="fab fa-twitter"></i
-            ></a>
+            <a
+              class="btn btn-outline-black btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i class="fab fa-twitter"></i>
+            </a>
 
-            <a class="btn btn-outline-black btn-floating m-1" href="#!" role="button"
-            ><i class="fab fa-google"></i
-            ></a>
+            <a
+              class="btn btn-outline-black btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i class="fab fa-google"></i>
+            </a>
 
-            <a class="btn btn-outline-black btn-floating m-1" href="#!" role="button"
-            ><i class="fab fa-instagram"></i
-            ></a>
+            <a
+              class="btn btn-outline-black btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i class="fab fa-instagram"></i>
+            </a>
 
-            <a class="btn btn-outline-black btn-floating m-1" href="#!" role="button"
-            ><i class="fab fa-linkedin-in"></i
-            ></a>
+            <a
+              class="btn btn-outline-black btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i class="fab fa-linkedin-in"></i>
+            </a>
 
-            <a class="btn btn-outline-black btn-floating m-1" href="#!" role="button"
-            ><i class="fab fa-github"></i
-            ></a>
+            <a
+              class="btn btn-outline-black btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i class="fab fa-github"></i>
+            </a>
           </section>
 
           <section class="">
@@ -43,7 +66,12 @@ export default function Footer() {
 
                 <div class="col-md-5 col-12">
                   <div class="form-outline form-black mb-4">
-                    <input type="email" id="form5Example21" class="form-control" placeholder="Email " />
+                    <input
+                      type="email"
+                      id="form5Example21"
+                      class="form-control"
+                      placeholder="Email "
+                    />
                   </div>
                 </div>
 
@@ -58,8 +86,9 @@ export default function Footer() {
 
           <section class="mb-4">
             <p>
-              Here at Foodie.com we make sure to use healthy and fresh vegetables and cereals in making
-              best meals. Meaty dishes are prepared using meat of healthy animals bought from trusted firms.
+              Here at Foodie.com we make sure to use healthy and fresh
+              vegetables and cereals in making best meals. Meaty dishes are
+              prepared using meat of healthy animals bought from trusted firms.
               Give us a chance to give you best taste of your life 😊
             </p>
           </section>
@@ -67,9 +96,16 @@ export default function Footer() {
 
         <div class="text-center p-3" style={{ backgroundColor: "#FF9F0D" }}>
           © 2022 Copyright
-          <a class="text-white" style={{ textDecoration: "none" }} href="https://Foodie.com/"> Foodie.com</a>
+          <a
+            class="text-white"
+            style={{ textDecoration: "none" }}
+            href="https://Foodie.com/"
+          >
+            {" "}
+            Foodie.com
+          </a>
         </div>
       </footer>
     </div>
-  )
+  );
 }
